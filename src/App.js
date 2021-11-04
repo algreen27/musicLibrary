33c605f04667635960ca4956cd3222d784a02617
Al Green <a.l.green@hotmail.com>
2021-11-03 22:30:30 -0500

    initialize music project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import 
+
+import React, { Component } from 'react'
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App
+
